7ba737db818d723ef2758b0902591a70fed7efb1
Tommy Sprague <tommy.sprague@gmail.com>
2017-10-11 14:09:56 -0400

    updates to docs and temporal_task.sh
    Improved documentation overall (including README.md) and cleaned up
    temporal_task.sh, which now correctly  masks functional voxels and
    ensures aligned mask/functional data.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Prisma preprocessing procedures (AFNI) at NYU for Curtis Lab
 Goal of our preprocessing is to align all functional data to a common space (freesurfer's recon-all) at a common resolution, dictated by *task* data. However, retinotopic mapping seems to work a bit better at higher resolutions (less partial voluming), so part of this procedure involves sampling into different volumetric coordinate systems.
 
 ## Retinotopic mapping (vRF) data
-For identifying ROIs, we acquire retinotopic mapping scans using a protocol similar to that defined in Mackey, Winawer & Curtis, 2017, *eLife*. Participants attend to a moving bar presented at different widths, moving steadily across the screen in one of four directions (top-to-bottom,left-to-right,bottom-to-top,right-to-left) and perform a challenging discrimination task requiring attention to the entire bar. We repeat the same stimulus sequence 10-12x per participant and average over all scan timeseries, then estimate a receptive field for each voxel, using a GPU-accelerated branch of *vistasoft*.
+For identifying ROIs, we acquire retinotopic mapping scans using a protocol similar to that defined in [Mackey, Winawer & Curtis, 2017, *eLife*](https://elifesciences.org/articles/22974). Participants attend to a moving bar presented at different widths, moving steadily across the screen in one of four directions (top-to-bottom,left-to-right,bottom-to-top,right-to-left) and perform a challenging discrimination task requiring attention to the entire bar (stimulus scripts available [here](https://github.com/tommysprague/vRF_stim)). We repeat the same stimulus sequence 10-12x per participant and average over all scan timeseries, then estimate a receptive field for each voxel, using a GPU-accelerated branch of [*vistasoft*](https://github.com/tommysprague/vistasoft_ts/tree/sprague_gridfit_updates).
 
 We acquire data at 2.0 mm isovoxel resolution with a 1200 ms TR (multiband factor 4x) on a Siemens Prisma scanner. We also acquire high-resolution anatomy (0.8 mm isovoxel T1's and T2), and spin-echo blip up/down 'TOPUP'-style fieldmaps to correct for field distortions. We analyze RF datasets in several ways, each used at different stages of analyses. For ROI identification, we smooth data along the surface. For using RF parameters to sort voxels or perform other (multivariate) analyses, we only project data onto surface and back into volume space. By performing RF fitting and subsequent analyses in volume space after surface-based smoothing, we ensure we do not oversample the data, which can have substantial impacts on multivariate analyses (especially those concerned with correlations between voxels). For completeness, we also fit RFs to 'raw' data (after pre-processing) that was never projected onto the surface and thus still includes subcortical voxels. These data may be used for future purposes, but can also provide a good test of functional/anatomical alignment (do well-fit functional voxels fall within cortical ribbon defined anatomically?)
 
@@ -22,5 +22,9 @@ The script __preproc_RF.sh__ calls all necessary preprocessing commands to set u
 
 
 ## Task data
+When preprocessing task data (__preproc_task.sh__), we perform most of the above steps (though we leave out surface-smoothing), and add one other:
+1. __temporal_task.sh__ - detrends data and converts to % signal change
+
+Resulting data (both surface-ribbon, 'surf', and full-volume, 'func') can then be used by custom data processing scripts (typically in MATLAB/Python)
 
 Some functions (surface-->volume) require repository https://github.com/tommysprague/preproc_mFiles, which requires vistasoft (ideally this branch: https://github.com/tommysprague/vistasoft_ts/tree/sprague_gridfit_updates)
--- temporal_task.sh
+++ temporal_task.sh
@@ -2,41 +2,20 @@
 #
 # temporal_task.sh
 #
-# adapted/testing from temporal_task_tcs.sh
-#
 # GOALS:
 # [assumes afni_proc.py already run, we're working w/ volreg files]
-# TODO: incorporate support for surfsmooth'd data
-# TODO: incorporate multiple distortion correction iterations
 
 # This script:
-# 1) Deletes initial TRs
-# 2) Linear detrend
-# 3) Records timeseries mean
-# 4) Records global timeseries mean
-# 5) Filtering:
-#   a) detrend (polort = 2)
-#   b) highpass (f = 0.0119 Hz, 84 Seconds)
-#   c) regression of filtered mean global signal
-#   d) smooth within brain mask
-# 6) Normalize: mean (% signal change) & variance
-#
+# 1) resample to RAI orientation
+# 2) saves out a voxel-wise mean
+# 3) linear detrend
+# 4) convert to PSC
 
 
+# can process either "func" or "surf" files (4th arg), which have already been pre-processed
 
-## SETTINGS ###################################################################
-#CORES=12       # number of jobs (should equal the number of cores you have)
-
-#fSES=1          # first session number
-#SESS=1          # number of sessions
-#FWHM=0          # spatial smoothing (mm)
-#REWINDTRS=0     # number of TRs to end of rewind
-###############################################################################
 
 DATAROOT="/deathstar/data"
-#SUBJ="CC"
-
-
 
 EXPTDIR=$1
 
@@ -45,34 +24,11 @@ SESS=$3
 
 DATATYPE=$4 # surf or ${DATATYPE}
 
-
-
-
 ROOT=$DATAROOT/$EXPTDIR
 
-#declare -a SESSIONS=("MGSMap25mm_MB4")
 
 # get in the right directory - root directory of a given subj
 cd $ROOT/$SUBJ
-
-#for ((s=fSES;s<=SESS;s++)); do
-#for s in "${SESSIONS[@]}"; do
-
-    # FUNCPRE="pb02.${SUBJ}_${SESS}*.r"
-    # FUNCSUF=".volreg+orig.BRIK"
-    # #FUNCSTR="pb02.${SUBJ}_${s}.r*.volreg+orig.BRIK"
-    #
-    # ## set number of runs for current session
-    # #RUN=`ls -l $SESS/raw/*${DATATYPE}* | wc -l`
-# CORES=`ls -l $SESS/${SUBJ}_${SESS}*.results/${FUNCPRE}*${FUNCSUF} | wc -l`
-    # rm ./list.txt; for ((i=1;i<=RUN;i++)); do printf "%02.f\n" $i >> ./list.txt; done
-    #
-    #
-    # # COPY BRIK/HEAD to nii/gz in super-directory
-    # cat ./list.txt | parallel -P $CORES \
-    # 3dcopy $SESS/${SUBJ}_${s}*_SEalign.results/${FUNCPRE}{}${FUNCSUF} $SESS/${DATATYPE}{}_volreg.nii.gz
-    #
-
 
 # figure out how many cores...
 
@@ -80,87 +36,51 @@ RUN=`ls -l $DATAROOT/$EXPTDIR/$SUBJ/$SESS/${DATATYPE}*_volreg.nii.gz | wc -l`
 rm ./list.txt; for ((i=1;i<=$RUN;i++)); do printf "%02.f\n" $i >> ./list.txt; done
 CORES=$RUN
 
-    # Linear detrend (for  mean estimation)
-    cat ./list.txt | parallel -P $CORES \
-    3dDetrend -prefix $SESS/${DATATYPE}_tmp_det{}.nii.gz\
-              -polort 1 $SESS/${DATATYPE}{}_volreg.nii.gz
+# convert to RAI
+cat ./list.txt | parallel -P $CORES \
+3dresample -prefix $SESS/${DATATYPE}{}_volreg.nii.gz \
+           -orient rai \
+           -overwrite  \
+           -inset $SESS/${DATATYPE}{}_volreg.nii.gz
 
-    cat ./list.txt | parallel -P $CORES \
-    3dTstat -prefix $SESS/${DATATYPE}_tmp_mean{}.nii.gz \
-            -mean $SESS/${DATATYPE}{}_volreg.nii.gz
+# Linear detrend (for  mean estimation)
+cat ./list.txt | parallel -P $CORES \
+3dDetrend -prefix $SESS/${DATATYPE}_tmp_det{}.nii.gz \
+          -polort 1 $SESS/${DATATYPE}{}_volreg.nii.gz
 
-    cat ./list.txt | parallel -P $CORES \
-    3dTstat -prefix $SESS/${DATATYPE}_tmp_detMean{}.nii.gz \
-            -mean $SESS/${DATATYPE}_tmp_det{}.nii.gz
+cat ./list.txt | parallel -P $CORES \
+3dTstat -prefix $SESS/${DATATYPE}_tmp_mean{}.nii.gz \
+        -mean $SESS/${DATATYPE}{}_volreg.nii.gz
 
-    cat ./list.txt | parallel -P $CORES \
-    3dcalc -prefix $SESS/${DATATYPE}_volreg_detrend{}.nii.gz \
-           -a $SESS/${DATATYPE}_tmp_det{}.nii.gz \
-           -b $SESS/${DATATYPE}_tmp_mean{}.nii.gz \
-           -c $SESS/${DATATYPE}_tmp_detMean{}.nii.gz \
-           -expr "'a+b-c'"
+cat ./list.txt | parallel -P $CORES \
+3dTstat -prefix $SESS/${DATATYPE}_tmp_detMean{}.nii.gz \
+        -mean $SESS/${DATATYPE}_tmp_det{}.nii.gz
 
-    rm $SESS/*${DATATYPE}_tmp*
+# add back in original mean
+cat ./list.txt | parallel -P $CORES \
+3dcalc -prefix $SESS/${DATATYPE}_volreg_detrend{}.nii.gz \
+       -a $SESS/${DATATYPE}_tmp_det{}.nii.gz \
+       -b $SESS/${DATATYPE}_tmp_mean{}.nii.gz \
+       -c $SESS/${DATATYPE}_tmp_detMean{}.nii.gz \
+       -overwrite \
+       -expr "'a+b-c'"
 
-    ## here, we're left with linear-detrended data at its original mean
+rm $SESS/*${DATATYPE}_tmp*
 
-
-    ## Voxel-wise mean over timeseries (this is identical tot he tmp-mean file removed above...)
-    cat ./list.txt | parallel -P $CORES \
-    3dTstat -prefix $SESS/${DATATYPE}_volreg_mean{}.nii.gz \
-            -mean $SESS/${DATATYPE}_volreg_detrend{}.nii.gz
-
-    # skullstrip each (note: this looked like it was missing some brain for KD); default -clfrac 0.5
-#    cat ./list.txt | parallel -P $CORES \
-#    3dSkullStrip -prefix $SESS/${DATATYPE}_brain{}.nii.gz -input $SESS/${DATATYPE}_detrend{}.nii.gz -push_to_edge
-
-    # create mask of that
-#    cat ./list.txt | parallel -P $CORES \
-#    3dAutomask -prefix $SESS/${DATATYPE}_mask{}.nii.gz -clfrac 0.1 $SESS/${DATATYPE}_brain{}.nii.gz
+## here, we're left with linear-detrended data at its original mean
 
 
+## Voxel-wise mean over timeseries (this is identical tot he tmp-mean file removed above...)
+cat ./list.txt | parallel -P $CORES \
+3dTstat -prefix $SESS/${DATATYPE}_volreg_mean{}.nii.gz -overwrite \
+        -mean $SESS/${DATATYPE}_volreg_detrend{}.nii.gz
 
 
-    # ## Global mean timeseries & derivative
-    # for ((i=1;i<=RUN;i++)); do
-    # 3dmaskave -mask SESS$SESS/${DATATYPE}_mask$i.nii.gz \
-    #     -quiet SESS$SESS/${DATATYPE}_detrend$i.nii.gz > SESS$SESS/params_globalMean$i.1D
-    # done
-
-    # bandpass + smooth
-    # cat ./list.txt | parallel -P $CORES \
-    #     3dTproject -prefix $SESS/${DATATYPE}_hipass{}.nii.gz \
-    #                -blur $FWHM \
-    #                -mask anat_EPI_mask.nii.gz \
-    #                -passband 0.008 99999 \
-    #                -input $SESS/${DATATYPE}_detrend{}.nii.gz
-
-    # percent signal change
-#    cat ./list.txt | parallel -P $CORES \
-#    3dcalc -prefix $SESS/${DATATYPE}_normPct{}.nii.gz \
-#           -a $SESS/${DATATYPE}_hipass{}.nii.gz \
-#           -b $SESS/${DATATYPE}_mean{}.nii.gz \
-#           -c anat_EPI_mask.nii.gz \
-#           -expr "'(a/b) * c * 100'"
-# -c anat_EPI_mask.nii.gz
-
-
-    # percent signal change - computed w/ detrended rather than hi-pass'd data; also remove 1
-    cat ./list.txt | parallel -P $CORES \
-    3dcalc -prefix $SESS/${DATATYPE}_volreg_normPctDet{}.nii.gz \
-           -a $SESS/${DATATYPE}_volreg_detrend{}.nii.gz \
-           -b $SESS/${DATATYPE}_volreg_mean{}.nii.gz \
-           -c surfanat_brainmask_master.nii.gz \
-           -expr "'((a/b)-1) * ispositive(c) * 100'"
-
-
-    # ensure we're in RAI - otherwise vista, etc, get pissed
-    cat ./list.txt | parallel -P $CORES \
-    3dresample -prefix $SESS/${DATATYPE}_volreg_normPctDet{}.nii.gz \
-               -orient rai \
-               -overwrite  \
-               -inset $SESS/${DATATYPE}_volreg_normPctDet{}.nii.gz
-
-
-    # save out a set of volumes depicting the mean (to comapre residual distortions)
-    #3dTcat -prefix $SESS/${SUBJ}_${s}_meanruns.nii.gz $SESS/${DATATYPE}_volreg_mean*.nii.gz
+# percent signal change - computed w/ detrended rather than hi-pass'd data; also remove 1
+cat ./list.txt | parallel -P $CORES \
+3dcalc -prefix $SESS/${DATATYPE}_volreg_normPctDet{}.nii.gz \
+       -a $SESS/${DATATYPE}_volreg_detrend{}.nii.gz \
+       -b $SESS/${DATATYPE}_volreg_mean{}.nii.gz \
+       -c surfanat_brainmask_master.nii.gz \
+       -overwrite \
+       -expr "' and(ispositive(c),ispositive(b)) * ((a/b) - 1) *  100'"
