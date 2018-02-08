6ce8ee9bdcdbbcc6661323c5a90f155844b52426
Tommy Sprague <tommy.sprague@gmail.com>
2018-02-08 18:17:43 -0500

    made label_retmap more generic

--- label_retmap.sh
+++ label_retmap.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 # write labels for AFNI briks (for RF maps, for example)
+#
+# Run like preproc_RF.sh:
+# $PREPROC/label_retmap.sh vRF_tcs CC RF1
+# 
+# Will try to generate ss5, surf, func, ss5_25mm and func_25mm maps - if these aren't found, errors will be spit out, but script will run
+#
+# NOTE: does not overwrite, so if you re-run model fits, need to manually delete .nii.gz files in RETRDIR
 
 
 
 ROOT="/deathstar/data/vRF_tcs"
-SUBJ=$1
-SESS=$2
-vistaDir=$ROOT/$SUBJ/$SESS/${SUBJ}_${SESS}_vista
+
+RFDIR=$1
+SUBJ=$2
+SESS=$3
+vistaDir=/deathstar/data/$RFDIR/$SUBJ/$SESS/${SUBJ}_${SESS}_vista
 RFdir=$vistaDir/Inplane/Original   # where the RF files are saved, incl. nii.gz (raw)
 
-
-#declare -a RF_prefix=("RF_func" "RF_surf" "RF_ss5" ) # "RF_ss5" "RF_surf") # "RF_pctDet_surf")
-declare -a RF_prefix=("RF_ss5") # "RF_ss5" "RF_surf") # "RF_pctDet_surf")
+declare -a RF_prefix=("RF_ss5" "RF_surf" "RF_func" "RF_surf_25mm" "RF_func_25mm")
 declare -a RF_suffix=("gFit" "sFit" "fFit")
 
 for p in "${RF_prefix[@]}"; do
